adb174877a22d51b2945adf189dcac0571b3a58f
Rodrigo <rodmentou@gmail.com>
2015-11-06 21:22:38 -0300

    Removed some code.

--- server.js
+++ server.js
@@ -1,36 +1,21 @@
 var express = require('express'),
 	app = express(),
 	morgan = require('morgan'),
-	mongoose = require('mongoose'),
 	bodyParser = require('body-parser'),
 	server = require('http').createServer(app),
-	io = require('socket.io')(server),
-	passport = require('passport'),
-	CronJob = require('cron').CronJob,
-	jwt = require('jsonwebtoken'),
-	ejwt = require('express-jwt');
+	CronJob = require('cron').CronJob;
 
+process.env.PORT = 8080;
+process.env.env = 'dev';
 
-var jwtCheck = ejwt({
-  secret: new Buffer('na8e2W6DPANiyFD55o8IpX-ygdeWxId6JQ7AWCaRI9psMq3RaagkCDpOyvr05o_t', 'base64'),
-  audience: 'kaW247PPvX8pfitQZWgTlSSFKNTJjmNL'
-});
-
-var config = require('./config');
-
-var port = process.env.PORT || config.serverPort;
-var env = process.env.NODE_ENV || config.serverEnv;
-var jwtSecret = config.jwtSecret;
-var dbUrl = config.dbUrl;
+var port = process.env.PORT;
+var env = process.env.NODE_ENV;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan(env));
 app.use(express.static(__dirname + '/public'));
-app.use(passport.initialize());
-app.use(passport.session());
-//app.use('/api', jwtCheck);
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -38,11 +23,6 @@ app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
   next();
 });
-
-
-mongoose.connect(dbUrl);
-
-
 
 
 var apiRouter = express.Router();
@@ -54,13 +34,9 @@ require('./routes/users')(apiRouter);
 require('./routes/1x1')(apiRouter);
 app.use('/api', apiRouter);
 
-var User = require('./public/models/user.js');
 //Increase all users HP
 new CronJob('*/10 * * * * *', function() {
-  User.update( {}, 
-  	{ $inc: { hp: 10 } }, {multi: true}, function () {
-  		console.log("Refresh free");
-  	});
+
 }, null, true, 'America/Los_Angeles');
 
 
