8d721cd252f9ee0c27318b0e8be395afd03e12ab
Rodrigo <rodmentou@gmail.com>
2015-12-02 19:14:56 -0300

    Trying to resolve CORS on Ionic

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.use(express.static(__dirname + '/public'));
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
